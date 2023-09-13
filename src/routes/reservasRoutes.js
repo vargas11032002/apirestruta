81dbc74eb62b86bdaeebbd2ceededd2cdcd4dc35
duvanmonroy <duvanmonroy1127@gmail.com>
2023-09-12 19:06:48 -0500

    jhonv soluciono error en respuesta de reservas por id

--- src/routes/reservasRoutes.js
+++ src/routes/reservasRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 import * as reservasController from '../controllers/reservasController.js';
 
 // Rutas para reservas
-router.get('/:id', reservasController.obtenerReservas);
 router.get('/', reservasController.obtenerReservas);
+router.get('/:id', reservasController.obtenerReserva);
 router.post('/', reservasController.crearReserva);
 router.put('/:id', reservasController.actualizarReserva);
 router.delete('/:id', reservasController.eliminarReserva);
